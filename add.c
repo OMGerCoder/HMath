e9dd3f3b04838e4d472372e459f54af223829986
Hugh <hughomg@icloud.com>
2019-04-17 11:59:37 -0400

    Included stdio to maybe fix errors

--- add.c
+++ add.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 int add(int one, int two)
 {
     return one+two;
 }
+
--- subtract.c
+++ subtract.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 int subtract(int one, int two)
 {
-    return a-b
+    return a-b;
 }
+
